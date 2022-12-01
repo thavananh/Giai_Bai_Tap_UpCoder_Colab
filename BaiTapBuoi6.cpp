d544b03d8f9b5af7ffbf55ba470c98f77d5b3fc0
DuyNguyen <4801104035@student.hcmue.edu.vn>
2022-12-02 00:13:14 +0700

    234

--- BaiTapBuoi6.cpp
+++ BaiTapBuoi6.cpp
@@ -4,6 +4,12 @@
 using namespace std;
 void nhapmang(int a[row][col], int n, int m);
 void xuatmang(int a[row][col], int n, int m);
+void In_So_Tren_Duong_Cheo_Chinh(int a[row][col], int n, int m);
+
+
+
+
+
 
 void nhapmang(int a[row][col], int n, int m)
 {
@@ -27,6 +33,35 @@ void xuatmang(int a[row][col], int n, int m) {
     }
 }
 
+
+
+void In_So_Tren_Duong_Cheo_Chinh(int a[row][col], int n, int m) {
+    if (n != m)
+    {
+        return;
+    }
+    int i = 0;
+    int j = 0;
+    while (i != n)
+    {
+        cout << a[i][j] << " ";
+        i++; j++;
+    }
+}
+
+void In_Cac_So_Nam_Tren_Bien(int a[row][col], int n, int m) {
+    for (size_t i = 0; i < n; i++)
+    {
+        // đặt if ở đây
+        for (size_t j = 0; j < m; i++)
+        {
+            
+        }
+        
+    }
+    
+}
+
 int main() {
     int a[row][col];
     int n = 0;
@@ -34,6 +69,10 @@ int main() {
     cin >> n;
     cin >> m;
     nhapmang(a, n, m);
-    
-    
+    //xuatmang(a, n, m);
+    cout << endl;
+    cout << endl;
+    In_So_Tren_Duong_Cheo_Chinh(a, m, n);
+
+    system("Pause");
 }
