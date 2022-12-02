dd47d42c86c52f8907819d75617ce9c55f76bf17
DuyNguyen <4801104035@student.hcmue.edu.vn>
2022-12-02 10:22:54 +0700

    123

--- BaiTapBuoi6.cpp
+++ BaiTapBuoi6.cpp
@@ -88,10 +88,96 @@ void In_Tong_Moi_Cot(int a[row][col], int n, int m) {
             sum = sum + a[j][i];
         }
         cout << sum << endl;
+    }   
+}
+
+void In_Hang_Co_Tong_Lon_Nhat(int a[row][col], int n, int m) {
+    int sumArr[row]; 
+    for (size_t i = 0; i < n; i++)
+    {
+        int sum = 0;
+        
+        for (size_t j = 0; j < m; j++)
+        {
+            sum = sum + a[i][j];   
+        }
+        sumArr[i] = sum;
     }
+    int max = sumArr[0];
+    for (size_t i = 1; i < n; i++)
+    {
+       if (sumArr[i] > max)
+       {
+            max = sumArr[i];
+       }
+        
+    }
+    cout << max << endl;
+    
     
 }
 
+void In_Cot_Co_Tong_Lon_Nhat(int a[row][col], int n, int m){
+    int sumArr[col];
+    for (size_t i = 0; i < m; i++)
+    {
+        int sum = 0;
+        for (size_t j = 0; j < n; j++)
+        {
+            sum = sum + a[j][i];
+        }
+        sumArr[i] = sum;
+    }
+    int min = sumArr[0];
+    for (size_t i = 0; i < n; i++)
+    {
+        if (sumArr[i] < min)
+        {
+            min = sumArr[i];
+        }
+    }
+    cout << min << endl;
+}
+
+void Sap_Xep_Thu_Tu_Tren_Moi_Hang(int a[row][col], int n, int m) {
+    for (size_t i = 0; i < n; i++)
+    {
+        for (size_t j = 0; j < m-1; j++)
+        {
+            for (size_t k = 0; k < m; k++)
+            {
+                int temp = 0;
+                if (a[i][k] < a[i][k + 1])
+                {
+                    temp = a[i][k];
+                    a[i][k] = a[i][k+1];
+                    a[i][k+1] = temp;
+                }
+                
+            }
+        }
+    }
+}
+
+void Sap_Xep_Thu_Tu_Tang_Dan_Tren_Moi_Cot(int a[row][col], int n, int m) {
+    for (size_t i = 0; i < m; i++)
+    {
+        for (size_t j = 0; j < n-1; j++)
+        {
+            for (size_t k = 0; k < n; k++)
+            {
+                int temp = 0;
+                if (a[k][i] < a[k+1][i])
+                {
+                    temp = a[k][i];
+                    a[k][i] = a[k+1][i];
+                    a[k+1][i] = temp;
+                }
+                
+            }
+        }
+    }
+}
 int main() {
     int a[row][col];
     int n = 0;
@@ -105,6 +191,11 @@ int main() {
     //In_So_Tren_Duong_Cheo_Chinh(a, n, m);
     //In_Cac_So_Nam_Tren_Bien(a, n, m);
     //In_Tong_Moi_Hang(a, n, m);
-    In_Tong_Moi_Cot(a, n, m);
+    //In_Tong_Moi_Cot(a, n, m);
+    //In_Hang_Co_Tong_Lon_Nhat(a, n, m);
+    //In_Cot_Co_Tong_Lon_Nhat(a, n, m);
+    //Sap_Xep_Thu_Tu_Tang_Dan_Tren_Moi_Hang(a, n, m);
+    Sap_Xep_Thu_Tu_Tang_Dan_Tren_Moi_Cot(a, n, m);
+    xuatmang(a, n, m);
     system("Pause");
 }
