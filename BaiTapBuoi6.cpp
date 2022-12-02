f59605a54c7fd562ba5aa1fe667c0403c76868fc
DuyNguyen <4801104035@student.hcmue.edu.vn>
2022-12-02 15:16:08 +0700

    123

--- BaiTapBuoi6.cpp
+++ BaiTapBuoi6.cpp
@@ -1,15 +1,51 @@
 #include <iostream>
+#include<cmath>
 #define row 100
 #define col 100
 using namespace std;
+
 void nhapmang(int a[row][col], int n, int m);
 void xuatmang(int a[row][col], int n, int m);
 void In_So_Tren_Duong_Cheo_Chinh(int a[row][col], int n, int m);
+void In_Cac_So_Nam_Tren_Bien(int a[row][col], int n, int m);
+void In_Tong_Moi_Hang(int a[row][col], int n, int m);
+void In_Tong_Moi_Cot(int a[row][col], int n, int m);
+void In_Hang_Co_Tong_Lon_Nhat(int a[row][col], int n, int m);
+void In_Cot_Co_Tong_Lon_Nhat(int a[row][col], int n, int m);
+void In_Ra_Gia_Tri_Be_Nhat_Moi_Cot(int a[row][col], int n, int m);
+void Sap_Xep_Thu_Tu_Tren_Moi_Hang(int a[row][col], int n, int m);
+void Sap_Xep_Thu_Tu_Tang_Dan_Tren_Moi_Cot(int a[row][col], int n, int m);
+void In_Ra_Cac_So_Nguyen_To_Moi_Hang(int a[row][col], int n, int m);
+bool Check_So_Nguyen_To(int n);
+bool Check_So_Chinh_Phuong(int n);
 
+bool Check_So_Chinh_Phuong(int n) {
+    int m = sqrt(n);
+    for (size_t i = 1; i <= sqrt(n); i++)
+    {
+        if (i*i == n)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
 
-
-
-
+bool Check_So_Nguyen_To(int n) {
+    if (n < 2)
+    {
+        return 0;
+    }
+    for (size_t i = 3; i <= sqrt(n); i++)
+    {
+        if (n%i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+    
+}
 
 void nhapmang(int a[row][col], int n, int m)
 {
@@ -32,8 +68,6 @@ void xuatmang(int a[row][col], int n, int m) {
         cout << endl;
     }
 }
-
-
 
 void In_So_Tren_Duong_Cheo_Chinh(int a[row][col], int n, int m) {
     if (n != m)
@@ -139,6 +173,37 @@ void In_Cot_Co_Tong_Lon_Nhat(int a[row][col], int n, int m){
     cout << min << endl;
 }
 
+void In_Ra_Gia_Tri_Lon_Nhat_Moi_Hang(int a[row][col], int n, int m){
+    for (size_t i = 0; i < n; i++)
+    {
+        int max = a[i][0];
+        for (size_t j = 1; j < m; j++)
+        {
+            if (a[i][j] > max)
+            {
+                max = a[i][j];
+            }
+        }
+        cout << max << endl;
+    }
+}
+
+void In_Ra_Gia_Tri_Be_Nhat_Moi_Cot(int a[row][col], int n, int m){
+    for (size_t i = 0; i < n; i++)
+    {
+        int min = a[0][i];
+        for (size_t j = 1; j < m; j++)
+        {
+            if (a[j][i] < min)
+            {
+                min = a[j][i];
+            }
+        }
+        cout << min << endl;
+    }
+    
+}
+
 void Sap_Xep_Thu_Tu_Tren_Moi_Hang(int a[row][col], int n, int m) {
     for (size_t i = 0; i < n; i++)
     {
@@ -178,6 +243,66 @@ void Sap_Xep_Thu_Tu_Tang_Dan_Tren_Moi_Cot(int a[row][col], int n, int m) {
         }
     }
 }
+
+void In_Ra_Cac_So_Nguyen_To_Moi_Hang(int a[row][col], int n, int m) {
+    for (size_t i = 0; i < n; i++)
+    {
+        for (size_t j = 0; j < m; j++)
+        {
+            if (Check_So_Nguyen_To(a[i][j]))
+            {
+                cout << a[i][j] << " ";
+            }
+        }
+        cout << endl;
+    }
+    
+}
+
+void In_Ra_Cac_So_Chinh_Phuong_Moi_Cot(int a[row][col], int n, int m) {
+    for (size_t i = 0; i < m; i++)
+    {
+        for (size_t j = 0; j < n; j++)
+        {
+            if (Check_So_Chinh_Phuong(a[j][i]))
+            {
+                cout << a[j][i] << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void In_Ra_So_Lon_Nhat_Cua_Mang(int a[row][col], int n, int m) {
+    int arrTemp[col];
+    for (size_t i = 0; i < n; i++)
+    {
+        for (size_t j = 0; j < m-1; j++)
+        {
+            for (size_t k = 0; k < m; k++)
+            {
+                int temp = 0;
+                if (a[i][k] < a[i][k+1])
+                {
+                    temp = a[i][k];
+                    a[i][k] = a[i][k+1];
+                    a[i][k+1] = temp;
+                }
+            }
+            arrTemp[i] = a[i][0];
+        }
+    }
+    int max = arrTemp[0];
+    for (size_t i = 1; i < n; i++)
+    {
+        if (arrTemp[i] > max)
+        {
+            max = arrTemp[i];
+        }
+    }
+    cout << max << endl;
+}
+
 int main() {
     int a[row][col];
     int n = 0;
@@ -185,17 +310,24 @@ int main() {
     cin >> n;
     cin >> m;
     nhapmang(a, n, m);
-    //xuatmang(a, n, m);
+    xuatmang(a, n, m);
     cout << endl;
     cout << endl;
-    //In_So_Tren_Duong_Cheo_Chinh(a, n, m);
-    //In_Cac_So_Nam_Tren_Bien(a, n, m);
-    //In_Tong_Moi_Hang(a, n, m);
-    //In_Tong_Moi_Cot(a, n, m);
-    //In_Hang_Co_Tong_Lon_Nhat(a, n, m);
-    //In_Cot_Co_Tong_Lon_Nhat(a, n, m);
-    //Sap_Xep_Thu_Tu_Tang_Dan_Tren_Moi_Hang(a, n, m);
+    In_So_Tren_Duong_Cheo_Chinh(a, n, m);
+    In_Cac_So_Nam_Tren_Bien(a, n, m);
+    In_Tong_Moi_Hang(a, n, m);
+    In_Tong_Moi_Cot(a, n, m);
+    In_Hang_Co_Tong_Lon_Nhat(a, n, m);
+    In_Cot_Co_Tong_Lon_Nhat(a, n, m);
+    In_Ra_Gia_Tri_Lon_Nhat_Moi_Hang(a, n, m);
+    In_Ra_Gia_Tri_Be_Nhat_Moi_Cot(a, n, m);
+    Sap_Xep_Thu_Tu_Tren_Moi_Hang(a, n, m);
+    xuatmang(a, n, m);
     Sap_Xep_Thu_Tu_Tang_Dan_Tren_Moi_Cot(a, n, m);
     xuatmang(a, n, m);
+    In_Ra_Cac_So_Nguyen_To_Moi_Hang(a, n, m);
+    In_Ra_Cac_So_Chinh_Phuong_Moi_Cot(a, n, m);
+    In_Ra_So_Lon_Nhat_Cua_Mang(a, n, m);
+
     system("Pause");
 }
