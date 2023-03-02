bb312aa8c6b56faab82b802734850fca93002e1e
DuySieuCuteHehe <thavananh@gmail.com>
2023-03-02 21:43:10 +0700

    123

--- QTaiMang.cpp
+++ QTaiMang.cpp
@@ -2,26 +2,50 @@
 using namespace std;
 struct Mang {
     int numberOfElemnt, array[100];
+    int* ptr;
     public:
-        // void operator = (const Mang &m) {
-        //     numberOfElemnt = m.numberOfElemnt;
-        //     for (size_t i = 0; i < m.numberOfElemnt; i++)
-        //     {
-        //         array[i] = m.array[i];
-        //     }
-        // }
+        void operator = (const Mang &m) {
+            numberOfElemnt = m.numberOfElemnt;
+            for (size_t i = 0; i < m.numberOfElemnt; i++)
+            {
+                array[i] = m.array[i];
+            }
+        }
+        int& operator[](int);
 };
 
-void Mang::operator = (const Mang &m);
-
+int operator + (Mang a, Mang b);
 istream& operator >> (istream &input, Mang &m);
 ostream& operator << (ostream &output, Mang m);
+bool operator == (Mang a, Mang b);
+bool operator != (Mang a, Mang b); 
 
 int main() {
     Mang m1, m2;
-    cin >> m1;
+    cin >> m1 >> m2;
+    if (m1 == m2)
+    {
+        cout << "yes";
+    }
+    else
+    {
+        cout << "no";
+    }
     system("Pause");
+}
 
+int operator + (Mang a, Mang b) {
+    int sum = 0;
+    for (size_t i = 0; i < a.numberOfElemnt; i++)
+    {
+       sum = sum + a.array[i]; 
+    }
+    for (size_t i = 0; i < b.numberOfElemnt; i++)
+    {
+       sum = sum + b.array[i]; 
+    }
+    return sum;
+    
 }
 
 istream& operator >> (istream &input, Mang &m) {
@@ -39,4 +63,59 @@ ostream& operator << (ostream &output, Mang m) {
         output << m.array[i] << " ";
     }
     return output;
+}
+
+bool operator == (Mang a, Mang b) {
+    if (a.numberOfElemnt != b.numberOfElemnt)
+    {
+        return false;
+    }
+    else  
+    {
+       for (size_t i = 0; i < a.numberOfElemnt; i++)
+       { 
+           if (a.array[i] == b.array[i])
+           {
+                continue;
+           }
+           else
+           {
+                return false;
+           }
+            
+       }
+       return true; 
+    }
+    
+}
+
+bool operator != (Mang a, Mang b) {
+    if (a.numberOfElemnt != b.numberOfElemnt)
+    {
+        return true;    
+    }
+    else
+    {
+        for (size_t i = 0; i < a.numberOfElemnt; i++)
+        {
+            if (a.array[i] != b.array[i])
+            {
+                return true; 
+            }
+            else
+            {
+                continue;
+            } 
+        }
+        return false; 
+    }
+}
+
+int& Mang::operator[](int index) {
+    if (index >= numberOfElemnt) {
+        cout << "Vuot qua so phan tu";
+        exit(0);
+    }
+    return ptr[index]; 
+    
 }
