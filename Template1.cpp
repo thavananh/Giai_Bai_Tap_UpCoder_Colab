888ee5d502acf33c425cc98ae73e175813399ba1
Nguyễn Võ Minh Duy <4801104035@student.hcmue.edu.vn>
2023-03-03 09:05:49 +0700

    123

---
+++ Template1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+template <typename T>
+void nhap();
+
+template <typename T>
+T timmax(T a, T b, T c);
+
+int main() {
+    
+}
+
+template <typename T>
+void nhap() {
+    T a, b, c;
+    cin >> a >> b >> c;
+}
+
+template <typename T>
+T timmax(T a, T b, T c) {
+    T _max = a;
+    if (b > max)
+    {
+        _max = b; 
+    }
+    if (c > max)
+    {
+        _max = c; 
+    }
+    return _max;
+    
+}
