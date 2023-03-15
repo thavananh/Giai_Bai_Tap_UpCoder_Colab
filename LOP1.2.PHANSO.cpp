bd8465746d9b2bb6943caac42f22b9efb67f30c1
Nguyễn Võ Minh Duy <4801104035@student.hcmue.edu.vn>
2023-03-15 14:05:36 +0700

    123

--- LOP1.2.PHANSO.cpp
+++ LOP1.2.PHANSO.cpp
@@ -6,41 +6,155 @@ class Phanso {
         int a;
         int b;
     public:
-        void nhap();
-        void xuat();
-        void nhapmauso();
-        void nhaptuso();
-        void phansonghichdao();
-        void rutgonphanso();
-        void quydongmauso(int&, int&);
-        void tinhtong();
-        int ucln(int&, int&);
-
+        Phanso();
+        Phanso(int, int);
+        // void nhap();
+        // void xuat();
+        // void phansonghichdao();
+        // void rutgonphanso();
+        // void quydongmauso(int&, int&);
+        // void tinhtong();
+        int getTuSo();
+        int getMauSo();
+        void setTuSo(int);
+        void setMauSo(int);
+        friend int ucln(int, int);
+        Phanso rutgon();
+        Phanso nghichdao();
+        Phanso operator + (Phanso);
+        Phanso operator * (Phanso);
+        Phanso operator / (Phanso);
+        friend istream& operator >> (istream &, Phanso &);
+        friend ostream& operator << (ostream &, Phanso );
 
 };
-void Phanso::nhap() {
-    cin >> this->a;
-    cin >> this->b;
+
+Phanso::Phanso() {
+    this->a = this->b = 0;
 }
-void Phanso::xuat(){
-    cout << this->a << "/" << this->b << '\n';
+
+Phanso::Phanso(int a, int b) {
+    this->a = a;
+    this->b = b;
 }
-void Phanso::nhaptuso(){
-    int a = 0;
-    cin >> a;
-    cout << a << '\n';
+
+istream& operator >> (istream &is, Phanso &p) {
+    is >> p.a >> p.b;
+    return is;
 }
-void Phanso::nhapmauso(){
-    int b = 0;
-    cin >> b;
-    cout << b << '\n';
+
+ostream& operator << (ostream &os, Phanso p) {
+    os << p.a << "/" << p.b;
+    return os; 
 }
-void Phanso::phansonghichdao(){
-    cout << this->b << "/" << this->a << '\n';
+
+int ucln(int a, int b) {
+    while (a != 0 && b!= 0)
+    {
+        if (a > b)
+        {
+            a = a % b;
+        }
+        else
+        {
+            b = b % a;
+        }
+    }
+    if (a > 0)
+    {
+        return a; 
+    }
+    return b;
+     
 }
-void Phanso::rutgonphanso() {
+
+int Phanso::getTuSo() {
+    return this->a;
+}
+
+int Phanso::getMauSo() {
+    return this->b;
+}
+
+void Phanso::setTuSo(int a) {
+   this->a = a; 
+}
+
+void Phanso::setMauSo(int b) {
+    this->b = b;
+}
+
+Phanso Phanso::nghichdao() {
+    Phanso rs;
+    rs.a = this->b;
+    rs.b = this->a;
+    return rs;
+}
+
+Phanso Phanso::rutgon() {
+    Phanso rs;
+    int k = ucln(this->a, this->b);
+    rs.a = this->a / k;
+    rs.b = this->b / k;
+    return rs;
+}
+
+Phanso Phanso::operator + (Phanso p) {
+    Phanso rs;
+    rs.a = this->a * p.b + p.a * this->b;
+    rs.b = p.b * this->b;
+    int k = ucln(rs.a, rs.b);
+    rs.a = rs.a / k;
+    rs.b = rs.b / k;
+    return rs;
+}
+
+Phanso Phanso::operator * (Phanso p) {
+    Phanso rs;
+    rs.a = this->a * p.a;
+    rs.b = this->b * p.b;
+    int k = ucln(rs.a, rs.b);
+    rs.a = rs.a / k;
+    rs.b = rs.b / k;
+    return rs;
+}
+
+// void Phanso::phansonghichdao(){
+//     cout << this->b << "/" << this->a << '\n';
+// }
+// void Phanso::rutgonphanso() {
+//     int a = this->a;
+//     int b = this->b;
+//     int k = ucln(a, b);
+//     cout << a/k << "/" << b/k << '\n';
     
-}
+// }
+
+// void Phanso::tinhtong() {
+//     int a = this->a;
+//     int b = this->b;
+//     int c = this->b;
+//     int d = this->a;
+//     int temp1 = a*d + c*b;
+//     int temp2 = b * d;
+//     int k = ucln(temp1, temp2);
+//     cout << temp1/k << "/" << temp2/k << '\n';
+// }
+
 int main(){
+    Phanso p;
+    // p.nhap();
+    // p.xuat();
+    cin >> p;
+    cout << p << '\n';
+    cout << p.getTuSo() << '\n';
+    cout << p.getMauSo() << '\n';
+    cout << p.nghichdao() << '\n';
+    cout << p.rutgon() << '\n';
+    cout << p + p.nghichdao() << '\n';
+    // p.phansonghichdao();
+    // p.rutgonphanso();
+    // p.tinhtong();
+    system("Pause");
 
 }
