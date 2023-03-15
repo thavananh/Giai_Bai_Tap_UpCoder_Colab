8d14957bdcdde869eebed12645d3e9abef2b57bc
Nguyễn Võ Minh Duy <4801104035@student.hcmue.edu.vn>
2023-03-15 15:54:57 +0700

    123

---
+++ vector.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int>v;
+    int x;
+    while (cin >> x)
+    {
+        v.push_back(x);
+    }
+    for (auto it : v) {
+        while (it != 0)
+        {
+            
+        }
+        
+    }
+    
+}
