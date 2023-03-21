513e5c9ff6a3f51cd2ee9bc2b242010ee492dcda
DuySieuCuteHehe <thavananh@gmail.com>
2023-03-21 19:13:02 +0700

    123

--- vector2.cpp
+++ vector2.cpp
@@ -1,44 +1,47 @@
 #include <iostream>
 #include <vector>
+#include<cstring>
+#include<string>
+#include<sstream>
 using namespace std;
 
 int main() {
-    vector<int>v;
-    int x = 0;
-    while (cin >> x)
+    string s;
+    getline(cin, s);
+    stringstream ss(s);
+    string tmp;
+    int array[100];
+    int i = 0;
+    while (ss >> tmp)
     {
-        if(cin.get() == '\n') {
-            break;
-        }
-        v.push_back(x);
+        array[i] = stoi(tmp);
+        i++;
     }
     vector<int>v1;
     int k = 0;
     while (cin >> k)
     {
-        if(cin.get() == '\n') {
-            break;
-        }
         v1.push_back(k);
     }
-    if (v.size() > 1)
+    if (i > 1)
     {
-        v1.erase(v.begin(), v.begin()+1);
+        v1.erase(v1.begin() + array[0], v1.begin() + array[1]);
     }
     else
     {
-        if (v[0] < 0)
-        {
+        if (array[0] == -1) {
             v1.clear();
         }
-        else
-        {
-            v1.erase(v.begin() + v[0]);
+        else {
+            v1.erase(v1.begin() + array[0]);
         }
-        
     }
-    for(vector<int>::iterator it = v.begin(); it != v.end(); it++)
-    {
-        cout << *it << " ";
+    if (v1.size() == 0) {
+        cout << "empty" << '\n';
+    }
+    else {
+        for (auto it : v1) {
+            cout << it << " ";
+        }
     }
 }
