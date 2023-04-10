a2dc447022a364d15d6318d875d23c4fd44a1fb7
DuySieuCuteHehe <thavananh@gmail.com>
2023-04-10 16:19:53 +0700

    123

--- NGOACDUNG.cpp
+++ NGOACDUNG.cpp
@@ -1,4 +1,19 @@
-if (!stc.empty() && stc.top() != '[')
+#include <iostream>
+#include <stack>
+#include <cstring>
+using namespace std;
+
+int main() {
+    stack<char>stc;
+    string s;
+    cin >> s;
+    int count = 0;
+    for (size_t i = 0; i < s.size(); i++)
+    {
+        switch (s[i])
+        {
+        case ')': 
+            if (!stc.empty() && stc.top() != '(')
             {
                 stc.push(s[i]);
             }
@@ -7,3 +22,37 @@ if (!stc.empty() && stc.top() != '[')
                 stc.pop();
             }
             break;
+        case ']':
+            if (!stc.empty() && stc.top() != '[')
+            {
+                stc.push(s[i]);
+            }
+            else
+            {
+                stc.pop();
+            }
+            break;
+        case '}':
+            if (!stc.empty() && stc.top() != '{')
+            {
+                stc.push(s[i]);
+            }
+            else
+            {
+                stc.pop();
+            }
+            break;
+        default:
+            stc.push(s[i]);
+            break;
+        }
+    }
+    if (stc.empty())
+    {
+        cout << "yes";
+    }
+    else
+    {
+        cout << "no";
+    }
+}
