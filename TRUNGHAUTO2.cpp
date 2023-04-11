8d050cff9ca9050793473f8f0a703505706dbe40
DuySieuCuteHehe <thavananh@gmail.com>
2023-04-11 13:46:03 +0700

    123

---
+++ TRUNGHAUTO2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <stack>
+#include <queue>
+using namespace std;
+
+int main() {
+    stack<char>stc;
+    char input[1000000];
+    while (scanf("%s", input) != EOF)
+    {
+        switch (input[0])
+        {
+        case '(':
+            
+            
+            break;
+        
+        default:
+            break;
+        }
+    }
+    
+}
