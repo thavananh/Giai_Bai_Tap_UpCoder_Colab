7218d134c01b1410ee1e4d4179e2e45618b04ded
DuySieuCuteHehe <thavananh@gmail.com>
2023-04-24 00:06:29 +0700

    123

---
+++ DAONGUOC.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int sum = 0;
+    while (n)
+    {
+        sum = (sum + n % 10)    *10;
+        n = n / 10; 
+    }
+    cout << sum;
+}
