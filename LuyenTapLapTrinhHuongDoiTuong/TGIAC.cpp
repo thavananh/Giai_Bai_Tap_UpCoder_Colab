eb77feab42903eacf1269e65b32df966513f6de8
DuySieuCuteHehe <thavananh@gmail.com>
2023-04-23 18:05:14 +0700

    123

--- LuyenTapLapTrinhHuongDoiTuong/TGIAC.cpp
+++ LuyenTapLapTrinhHuongDoiTuong/TGIAC.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 class Diem {
@@ -50,10 +51,58 @@ ostream &operator << (ostream &os, Diem d) {
     return os;
 }
 
-float Diem::khoangCach() {
+float Diem::khoangCach(Diem d2) {
+    return sqrt(pow(d2.hoanhDo - this->hoanhDo, 2) + pow(d2.tungDo - this->tungDo, 2));
+}
+
+bool Diem::operator== (Diem d2) {
+    if (this->hoanhDo == d2.hoanhDo && this->tungDo == d2.tungDo)
+    {
+        return true;
+    }
+    return false;
+}
+
+TamGiac::TamGiac(Diem d1, Diem d2, Diem d3) {
+    this->d1 = d1;
+    this->d2 = d2;
+    this->d3 = d3;
+}
+
+TamGiac::TamGiac(const TamGiac &tg) {
+    this->d1 = tg.d1;
+    this->d2 = tg.d2;
+    this->d3 = tg.d3;
+}
+
+TamGiac::~TamGiac() {}
+
+istream &operator >> (istream &is, TamGiac &tg) {
+    is >> tg.d1 >> tg.d2 >> tg.d3;
+    return is;
+}
+
+ostream &operator << (ostream &os, TamGiac tg) {
+    os << tg.d1 << tg.d2 << tg.d3;
+    return os;
+}
+
+int TamGiac::operator + (TamGiac tg2) {
+    return *this->chuVi() + tg2.chuVi();
+}
+
+bool TamGiac::operator < (TamGiac tg2) {
+
+}
+
+bool TamGiac::operator == (TamGiac tg2) {
 
 }
 
 int main() {
-
+    TamGiac tg1, tg2;
+    cin >> tg1 >> tg2;
+    cout << tg1 << '\n';
+    cout << tg2 << '\n';
+    system("Pause");
 }
