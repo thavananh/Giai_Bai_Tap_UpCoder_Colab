69431534cf2e7f15d82c1a31eddf208a19fc686a
thavananh <thavananh@gmail.com>
2023-05-18 12:39:44 +0700

    123

---
+++ ThuNghiemDeQuy/timMaxTrongSoN.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+long long timMaxTrongSoN(long long n, long long rs = INT_MIN)
+{
+    if (n == 0)
+    {
+        return rs;
+    }
+    int k = n % 10;
+    if (k > rs)
+    {
+        rs = k;
+    }
+    return timMaxTrongSoN(n/10, rs);
+}
+
+int main()
+{
+    long long n;
+    cin >> n;
+    cout << timMaxTrongSoN(n);
+    system("Pause");
+}
