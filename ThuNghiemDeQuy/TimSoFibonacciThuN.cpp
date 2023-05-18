927c7029ceb2f91018b471515fdd2e306317297e
thavananh <thavananh@gmail.com>
2023-05-18 21:22:43 +0700

    123

--- ThuNghiemDeQuy/TimSoFibonacciThuN.cpp
+++ ThuNghiemDeQuy/TimSoFibonacciThuN.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+long long fibonacci(long long n)
+{
+    if (n == 1 || n == 0)
+    {
+        return n;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
 int main()
 {
-    
+    long long n;
+    cin >> n;
+    cout << fibonacci(n);
 }
