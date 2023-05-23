09989c5cbbd5d7d92f926bb8f01df17bf5ded41e
thavananh <thavananh@gmail.com>
2023-05-23 22:57:20 +0700

    123

---
+++ HPalindrome.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ifstream FILE_IN("input.txt");
+    if (FILE_IN.is_open())
+    {
+        string line;
+        
+    }
+    
+}
