ed55855e63655bc73f864f1a05419c9b7cdd5014
thavananh <thavananh@gmail.com>
2023-05-29 15:34:27 +0700

    123

--- ThuNghiemDeQuy/ThuNghiemLyThuyetDoThi/TIMDUONG.cpp
+++ ThuNghiemDeQuy/ThuNghiemLyThuyetDoThi/TIMDUONG.cpp
@@ -5,44 +5,50 @@ using namespace std;
 
 void addEgde(vector<vector<int>>&m_adj, int u, int v)
 {
-    m_adj[u].push_back(v);
-    m_adj[v].push_back(u);
+    m_adj[u-1].push_back(v-1);
+    m_adj[v-1].push_back(u-1);
 }
 
-void BFS(vector<vector<int>>m_adj, int s, vector<int>&count, int end)
+int BFS(vector<vector<int>>m_adj, int s, int canh, int t)
 {
     queue<int>q;
-    vector<bool>visited(5, false);
-    q.push(s);
+    vector<bool>visited(canh, false);
+    vector<int>sum(canh, 0);
+    q.push(s-1);
     visited[s] = true;
     while (!q.empty())
     {
         int v = q.front();
         q.pop();
+        if (v == t-1)
+        {
+            return sum[t-1];
+        }
+        
         for (int k : m_adj[v])
         {
             if (!visited[k])
             {
+                sum[k] = sum[v] + 1;
                 q.push(k);
                 visited[k] = true;
             }
         }
     }
+    return -1;
 }
 
 int main()
 {
-    int dinh, canh, s, e;
-    cin >> dinh >> canh >> s >> e;
-    vector<vector<int>>m_adj(1000);
-    vector<bool>flag(100, false);
-    int count = 0;
+    int dinh, canh, s, t;
+    cin >> dinh >> canh >> s >> t;
+    vector<vector<int>>m_adj(10000);
     for (size_t i = 0; i < canh; i++)
     {
         int a, b;
         cin >> a >> b;
         addEgde(m_adj, a, b);
     }
-    
+    cout << BFS(m_adj, s, canh, t);
     return 0;
 }
