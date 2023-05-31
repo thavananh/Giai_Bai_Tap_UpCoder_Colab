2250112188630da488780912a8812543d95df5b6
thavananh <thavananh@gmail.com>
2023-06-01 01:09:43 +0700

    123

--- OnTapThiHK2_LapTrinh_FULL/PSOHONSO.cpp
+++ OnTapThiHK2_LapTrinh_FULL/PSOHONSO.cpp
@@ -1,146 +1,119 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-struct PhanSo
-{
-    int tu, mau;
+struct PhanSo {
+    int a, b;
 };
 
-istream &operator >> (istream &is, PhanSo &ps)
-{
-    is >> ps.tu >> ps.mau;
+struct HonSo {
+    int a;
+    PhanSo p;
+};
+
+istream& operator>>(istream& is, PhanSo& p) {
+    is >> p.a >> p.b;
     return is;
 }
 
-ostream &operator << (ostream &os, PhanSo ps)
-{
-    os << ps.tu << "/" << ps.mau;
+ostream& operator<<(ostream& os, const PhanSo& p) {
+    os << p.a << "/" << p.b;
     return os;
 }
 
-int ucln(int a, int b)
-{
-    if (b == 0)
-    {
+bool operator!=(const PhanSo& p1, const PhanSo& p2) {
+    return (p1.a != p2.a || p1.b != p2.b);
+}
+
+istream& operator>>(istream& is, HonSo& s) {
+    is >> s.a >> s.p;
+    return is;
+}
+
+ostream& operator<<(ostream& os, const HonSo& s) {
+    os << s.a << " " << s.p;
+    return os;
+}
+
+int ucln(int a, int b) {
+    while (a != 0 && b != 0) {
+        if (a > b) {
+            a %= b;
+        }
+        else {
+            b %= a;
+        }
+    }
+    if (a > 0) {
         return a;
     }
-    return ucln(b, a%b);
+    return b;
 }
 
-PhanSo operator + (PhanSo ps1, PhanSo ps2)
-{
+PhanSo rutGonPhanSo(const PhanSo& p) {
+    PhanSo rs = p;
+    int k = ucln(rs.a, rs.b);
+    rs.a /= k;
+    rs.b /= k;
+    return rs;
+}
+
+HonSo rutGonHonSo(const HonSo& s) {
+    HonSo rs = s;
+    rs.p = rutGonPhanSo(rs.p);
+    rs.a += rs.p.a / rs.p.b;
+    rs.p.a %= rs.p.b;
+    rs.p = rutGonPhanSo(rs.p);  // Tối giản kết quả của phần số trong hỗn số
+    return rs;
+}
+
+PhanSo chuyenDoiHonSo(const HonSo& s) {
     PhanSo rs;
-    rs.tu = ps1.tu * ps2.mau + ps2.tu * ps1.mau;
-    rs.mau = ps1.mau * ps2.mau;
-    int k = ucln(rs.tu, rs.mau);
-    rs.tu /= k;
-    rs.mau /= k;
+    rs.a = s.a * s.p.b + s.p.a;
+    rs.b = s.p.b;
+    rs = rutGonPhanSo(rs);  // Tối giản kết quả của phân số trong hỗn số
     return rs;
 }
 
-bool operator != (PhanSo ps1, PhanSo ps2)
-{
-    return ps1.tu != ps2.tu && ps1.mau != ps2.mau;
+bool operator!=(const HonSo& s1, const HonSo& s2) {
+    return (s1.a != s2.a || s1.p != s2.p);
 }
 
-struct HonSo
-{
-    int a;
-    PhanSo ps;
-};
-
-istream &operator >> (istream &is, HonSo &hs)
-{
-    is >> hs.a >> hs.ps;
-    return is;
+int tongCacThanhPhan(const HonSo& s) {
+    return s.a + s.p.a + s.p.b;
 }
 
-ostream &operator << (ostream &os, HonSo hs)
-{
-    os << hs.a << " " << hs.ps;
-    return os;
+bool operator>(const HonSo& s1, const HonSo& s2) {
+    return tongCacThanhPhan(s1) > tongCacThanhPhan(s2);
 }
 
-HonSo rutGonHonSo(HonSo hs)
-{
-    HonSo rs;
-    PhanSo p1, p2, p3;
-    p1.tu = hs.a;
-    p1.mau = 1;
-    p2.tu = hs.ps.tu;
-    p2.mau = hs.ps.mau;
-    p3 = p1 + p2;
-    if ((double)p3.tu / p3.mau)
-    {
-        rs.a = p3.tu / p3.mau;
-        rs.ps.tu = p3.tu % p3.mau;
-        rs.ps.mau = p3.mau;
-    }
-    else
-    {
-        rs.a = 0;
-        rs.ps.tu = p3.tu;
-        rs.ps.mau = p3.mau;
-    }
-    return rs;
-}
+int main() {
+    HonSo s1, s2;
+    string choices;
+    cin >> s1 >> s2 >> choices;
 
-PhanSo chuyenDoiPhanSo(HonSo hs)
-{
-    PhanSo rs;
-    PhanSo ps1, ps2;
-    ps1.tu = hs.a;
-    ps1.mau = 1;
-    ps2.tu = hs.ps.tu;
-    ps2.mau = hs.ps.mau;
-    rs = ps1 + ps2;
-    return rs;
-}
-
-int tongCacThanhPhan(HonSo hs)
-{
-    return hs.a + hs.ps.tu + hs.ps.mau;
-}
-
-bool operator != (HonSo hs1, HonSo hs2)
-{
-    return hs1.a != hs2.a || hs1.ps.tu != hs2.ps.tu || hs1.ps.mau != hs2.ps.mau;
-}
-
-bool operator > (HonSo hs1, HonSo hs2)
-{
-    return tongCacThanhPhan(hs1) > tongCacThanhPhan(hs2);
-}
-
-int main()
-{
-    HonSo hs1, hs2;
-    bool flag;
-    cin >> hs1 >> hs2 >> flag;
-    if (hs1 != hs2)
-    {
+    if (s1 != s2) {
         cout << "TRUE" << '\n';
     }
-    else
-    {
+    else {
         cout << "FALSE" << '\n';
     }
-    if (hs1 > hs2)
-    {
+
+    if (s1 > s2) {
         cout << "TRUE" << '\n';
     }
-    else
-    {
+    else {
         cout << "FALSE" << '\n';
     }
-    if (flag)
-    {
-        cout << rutGonHonSo(hs1) << '\n';
-        cout << rutGonHonSo(hs2) << '\n';
+
+    if (choices == "true") {
+        cout << rutGonHonSo(s1) << '\n';
+        cout << rutGonHonSo(s2) << '\n';
     }
-    else
-    {
-        cout << chuyenDoiPhanSo(hs1) << '\n';
-        cout << chuyenDoiPhanSo(hs2) << '\n';
+    else {
+        cout << chuyenDoiHonSo(s1) << '\n';
+        cout << chuyenDoiHonSo(s2) << '\n';
     }
+
+    return 0;
 }
