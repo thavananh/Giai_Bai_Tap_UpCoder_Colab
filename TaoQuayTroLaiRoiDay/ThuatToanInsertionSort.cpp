11718b7a20f5cd389343cefd4ac946b64e1f1ac8
duysieucutehehe123 <thavananh@gmail.com>
2023-10-04 12:29:56 +0700

    123123

---
+++ TaoQuayTroLaiRoiDay/ThuatToanInsertionSort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[] = {8, 7, 6, 5, 4, 3, 2, 1};
+    int n = 8;
+    for (size_t i = 1; i < n; i++)
+    {
+        for (size_t j = i; j > 0 && a[j-1] > j  nbbbbbbbbbbbbbbbb                                                                                                                                                 ; j++)
+        {
+            e
+        }
+        
+    }
+        
+}
---
+++ TaoQuayTroLaiRoiDay/ThuatToanInterchangeShort.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// sự khác nhau giữa interchange sort và bubble sort, đó là bubble luôn so sánh theo cặp của nó, ví dụ nó sẽ đi là a[j] và a[j+1] hoặc a[j] và a[j-1]
+//Interchange sort thì sẽ đi so sánh tuần tự, so sánh a[i] và a[j = i+1]
+
+int main()
+{
+    cout << "Hello World";
+}
